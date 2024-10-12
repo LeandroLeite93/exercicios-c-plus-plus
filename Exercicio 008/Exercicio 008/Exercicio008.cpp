4b2e8fb0d582e3a4f94805080bf34c5dc24ffcaf
LeandroLeite93 <leandro93.git@gmail.com>
2024-10-12 18:13:29 -0300

    adicionado mais um exercicio

--- Exercicio 008/Exercicio 008/Exercicio008.cpp
+++ Exercicio 008/Exercicio 008/Exercicio008.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
-    
+	setlocale(LC_ALL, "Portuguese");
+	float ValorHora, ValorTrabalhas, Resultado;
+
+	std::cout << "Quando você ganha por hora?: ";
+	std::cin >> ValorHora;
+
+	std::cout << "Quando horas você trabalha por dia?: "; 
+	std::cin >> ValorTrabalhas;
+
+	Resultado = ValorHora * ValorTrabalhas;
+
+	std::cout << "Total do seu Salario no mes é: " << Resultado << std::endl;
 
 	return 0;
 }
